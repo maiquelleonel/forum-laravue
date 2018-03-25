6884b9d7a4a803a1a56dd8c0804fa7b6e533d14e
Maiquel Leonel <skywishmtfk@gmail.com>
2018-03-25 14:54:16 -0300

    Revert "total_replies com observer"
    This reverts commit 0fd8b0694967c8771cef4f11a858c9fa194c5aa5.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use App\Reply;
-use App\Observers\ReplyObserver;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -16,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Reply::observe(ReplyObserver::class);
+        //
     }
 
     /**
